2bfb537b3aa474a2778daceb675ddce5e64838c3
RJ9496 <ruturajjamdar123@gmail.com>
2022-05-03 10:55:19 +0530

    1 file modified

--- src/app/e-info/e-info.component.ts
+++ src/app/e-info/e-info.component.ts
@@ -1,31 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import {DataService} from '../data.service';
+import { DataService } from '../data.service';
+
 @Component({
   selector: 'app-e-info',
   templateUrl: './e-info.component.html',
   styleUrls: ['./e-info.component.css'],
-  providers:[DataService],
-  })
-
+  providers: [DataService],
+})
 export class EInfoComponent implements OnInit {
-  inforecieved1: string []=[];
-  inforecieved2: string []=[];
-  inforecieved3:string []=[];
+  inforecieved1: string[] = [];
+  inforecieved2: string[] = [];
+  inforecieved3: string[] = [];
 
-  getinfofromService1(){
-    this.inforecieved1=this.dservice.getInfo1();
-
+  getinfofromService1() {
+    this.inforecieved1 = this.dservice.getInfo1();
   }
-  getinfofromService2(){
-    this.inforecieved2=this.dservice.getInfo2();
+  getinfofromService2() {
+    this.inforecieved2 = this.dservice.getInfo2();
   }
-  getinfofromService3(){
-    this.inforecieved3=this.dservice.getInfo3();
-  }
-
-  constructor(private dservice:DataService) { }
-
-  ngOnInit() {
+  getinfofromService3() {
+    this.inforecieved3 = this.dservice.getInfo3();
   }
 
+  constructor(private dservice: DataService) {}
+
+  ngOnInit() {}
 }
